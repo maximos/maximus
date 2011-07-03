maximos/maximus

commit e0c09b3b76e88124897f4592b7415ac62be9f05c

Removed unused code
refs gh-6

src/impl/install.bmx
@@ -123,7 +123,6 @@ Type mxInstallImpl Extends dArgumentImplementation
 		returns: Nothing.
 	End Rem
 	Method CheckDependencies:Int()
-		Local addlist:TListEx, nfounds:dObjectMap = New dObjectMap
 		For Local instmod:mxInstModule = EachIn m_instmap.ValueEnumerator()
 			CheckModuleDependencies(instmod, nfounds)
 		Next

commit 254cec7bb89c626463688210300de82a9a7a52fd

Now builds again
e0c09b3b76e88124897f4592b7415ac62be9f05c broke the build by removing a bit
too much

refs gh-15, gh-6

src/impl/install.bmx
@@ -123,6 +123,7 @@ Type mxInstallImpl Extends dArgumentImplementation
 		returns: Nothing.
 	End Rem
 	Method CheckDependencies:Int()
+		Local nfounds:dObjectMap = New dObjectMap
 		For Local instmod:mxInstModule = EachIn m_instmap.ValueEnumerator()
 			CheckModuleDependencies(instmod, nfounds)
 		Next